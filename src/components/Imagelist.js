4f36cd4ba62a67f02fe44755f3e359619337dc4c
Sunil Karki <karkisunil1200@gmail.com>
2020-01-11 13:14:41 -0600

    Added ImageCard componenet

--- src/components/Imagelist.js
+++ src/components/Imagelist.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 
 import './Imagelist.css';
 const Imagelist = props => {
-  const image = props.images.map(({description, id, urls}) => (
-    <img src={urls.regular} alt={description} key={id} />
-  ));
+  const image = props.images.map(image => <ImageCard image={image} key={image.id} />);
 
   return <div className='image-list'>{image}</div>;
 };
