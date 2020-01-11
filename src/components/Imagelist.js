3e8bca3ed871c92a8e93d0b32ed8bcfe39467705
Sunil Karki <karkisunil1200@gmail.com>
2020-01-11 14:39:26 -0600

    Added dynamic styling on cardlist component

--- src/components/Imagelist.js
+++ src/components/Imagelist.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ImageCard from './ImageCard';
 
 import './Imagelist.css';
+
 const Imagelist = props => {
   const image = props.images.map(image => <ImageCard image={image} key={image.id} />);
 
